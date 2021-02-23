3498619837161dd9c389bc246a3306df6dc4f3a7
Ludovic Geran <ludo@macbook-pro-de-ludovic.home>
2021-02-23 23:23:38 +0100

    commit project2

--- src/App.js
+++ src/App.js
@@ -18,16 +18,6 @@ const allumettes = [
 
 
 
-/*
-1. De créer un composant qui affiche le tableau 'allumettes'
-2. Créer des input box pour récupérer les saisies de l'utilisateur
-3. Checker les saisies avec les fonctions de check comme dans getUserInput
-4. Ensuite appliquer les changements sur les allumettes du state avec updateAllumettes
-4. Un bouton pour terminer le tour
-5. Quand c'est au tour de l'IA, il faut que tu utilises la fonction getBotInput pour avoir les inputs de l'IA
-6. Utiliser la fonction gameIsDone à chaque tour pour vérifier l'état de la partie
-7. Proposer un bouton pour reset la partie (optionnel)
-*/
 
 class App extends Component {
   constructor() {
